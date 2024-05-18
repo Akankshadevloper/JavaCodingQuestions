0c2d52096fb030601377951f0ae84a5cf3f3f0f0
Akanksha Kumari <200301120008@cutm.ac.in>
2024-05-19 01:25:33 +0530

    added new file

---
+++ src/FibonacciSequence.java
@@ -0,0 +1,19 @@
+public class FibonacciSequence {
+    public static void printFibonacciSequence(int count) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print(a + ", ");
+
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+
+    public static void main(String[] args) {
+        printFibonacciSequence(10);
+    }
+}
