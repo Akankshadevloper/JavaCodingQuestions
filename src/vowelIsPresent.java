43cdb72e9d7cedd853b292b479b83267d7478bb3
Akanksha Kumari <200301120008@cutm.ac.in>
2024-05-19 01:03:36 +0530

    one more question is added

---
+++ src/vowelIsPresent.java
@@ -0,0 +1,10 @@
+public class vowelIsPresent {
+    public static void main(String[] args){
+        System.out.println(stringContainsVowels("Hello")); // true
+        System.out.println(stringContainsVowels("TV")); // false
+    }
+
+    public static boolean stringContainsVowels(String input) {
+        return input.toLowerCase().matches(".*[aeiou].*");
+    }
+}
